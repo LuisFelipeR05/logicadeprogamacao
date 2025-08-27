dd2dc4c1a9014c4284b793574cbc29c8e1bbccd5
Luis Felipe <luisfelieprsantos05@outlook.com>
2025-08-27 17:16:26 -0300

    teste.py

---
+++ aula4/main.py
@@ -0,0 +1,29 @@
+import random
+
+numero_aleatorio = random.randint(1,100)
+palpite_jogador = int(input('Digite um numero_inteiro de 1 a 100: '))
+numero_tentativas = 1
+tentativas_passadas = []
+
+while numero_aleatorio != palpite_jogador:
+
+    if palpite_jogador in tentativas_passadas:
+        print('Você já tentou esse número, tente outro')
+    else:
+
+        tentativas_passadas.append(palpite_jogador)
+
+
+        numero_tentativas += 1
+
+        if palpite_jogador < numero_aleatorio:
+            print('O número é maior')
+        else:
+            palpite_jogador > numero_aleatorio
+            print('O número é menor')
+            
+        palpite_jogador = int(input("Digite um novo numero "))
+
+
+print(f"acertou em {numero_tentativas} tentativas")
+print(tentativas_passadas)
