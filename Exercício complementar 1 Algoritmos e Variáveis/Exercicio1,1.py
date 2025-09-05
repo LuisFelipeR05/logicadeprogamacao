2435d745c02e6ad2f21935593dbf71a60a8128a0
LuisFelipeR05 <luisfelipersantos05@outlook.com>
2025-09-05 13:02:42 -0300

    05/09/25

---
+++ Exercício complementar 1 Algoritmos e Variáveis/Exercicio1,1.py
@@ -0,0 +1,6 @@
+a = 7
+b = 5
+
+while True:
+    print("A é maior" if a > b else "B é maior")
+    break
---
+++ Exercício complementar 1 Algoritmos e Variáveis/Exercicio1.py
@@ -0,0 +1,9 @@
+a = 7
+b = 5
+
+if a > b:
+    print("A é maior")
+elif a == b:
+    print("A e B são iguais")
+else:
+    print("B é maior")
