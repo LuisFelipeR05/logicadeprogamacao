646627e44025cb81c5dee590dc259b0a65425d8c
LuisFelipeR05 <luisfelipersantos05@outlook.com>
2025-09-05 11:52:17 -0300

    lup.py

--- aula5/exemplo3.py
+++ aula5/exemplo3.py
@@ -8,36 +8,66 @@ while True:
     print("1-faça seu cadastro de usuario:")
     print("2-digite seu nome:")
     print("3-concluir cadastro")
-    print("4-usuario cadastrado com sucesso")
-    print("5-atualizar cadastro")
-    print("6-deletar cadastro")
-    print("7-listar usuarios cadastrados")
-    print("8-sair do sistema")
+    print("4-atualizar cadastro")
+    print("5-deletar cadastro")
+    print("6-listar usuarios cadastrados")
+    print("7-sair do sistema")
 
-opção = input("Digite a opção desejada: ")
-if opção == "1":
-    nome = input("Digite seu nome: ")
-    usuariso.append(nome)
-    print("Usuario cadastrado com sucesso!")
-elif opção == "2":
-    for i, nome in enumerate(usuariso):
-        print(f"{i + 1}. {nome}")
-indice = int(input("Digite o número do usuário que deseja atualizar: ")) - 1
-novo_nome = input("Digite o novo nome: ")
+    opção = input("Digite a opção desejada: ")
 
-elif opção == "3":
-    for i, nome in enumerate(usuariso):
-        print(f"{i + 1}. {usuario}")
-    indice = int(input("Digite o número do usuário que deseja deletar: ")) - 1
-    usuariso.pop(indice)
-    print("Usuário deletado com sucesso!")
-elif opção == "4":
-    for i, nome in enumerate(usuariso):
-        print(f"{i + 1}. {nome}")
-elif opção == "5":
-    print("Saindo do sistema...")
-    break
-else:
-    print("Opção inválida. Tente novamente.")
-    usuariso[indice] = novo_nome
-    print("Usuário atualizado com sucesso!")
+    if opção == "1":
+        nome = input("Digite seu nome: ")
+        usuariso.append(nome)
+
+        print("Usuario cadastrado com sucesso!")
+
+    elif opção == "2":
+        for i, nome in enumerate(usuariso):
+            print(f"{i + 1}. {nome}")
+        indice = int(input("Digite o número do usuário que deseja atualizar: ")) - 1
+        if 0 <= indice < len(usuariso):
+            novo_nome = input("Digite o novo nome: ")
+            usuariso[indice] = novo_nome
+        
+            print("Usuário atualizado com sucesso!")
+        
+        else:
+            print("Índice inválido.")
+    elif opção == "3":
+        print("Cadastro concluído.")
+        for i, nome in enumerate(usuariso):
+            print(f"{i + 1}. {nome}")
+
+    elif opção == "4":
+        for i, nome in enumerate(usuariso):
+            print(f"{i + 1}. {nome}")
+        indice = int(input("Digite o número do usuário que deseja atualizar: ")) - 1
+        if 0 <= indice < len(usuariso):
+            novo_nome = input("Digite o novo nome: ")
+            usuariso[indice] = novo_nome
+        
+            print("Usuário atualizado com sucesso!")
+        
+        else:
+            print("Índice inválido.")
+    elif opção == "5":
+        for i, nome in enumerate(usuariso):
+            print(f"{i + 1}. {nome}")
+        indice = int(input("Digite o número do usuário que deseja deletar: ")) - 1
+        if 0 <= indice < len(usuariso):
+            usuariso.pop(indice)
+            print("Usuário deletado com sucesso!")
+        else:
+            print("Índice inválido.")
+
+    elif opção == "6":
+        print("Usuários cadastrados:")
+        for i, nome in enumerate(usuariso):
+            print(f"{i + 1}. {nome}")
+
+    elif opção == "7":
+        print("Saindo do sistema...")
+        break
+
+        
+            
