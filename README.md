aa7f9a5c448057b3091e0676035ff98d51c6caef
LuisFelipeR05 <luisfelipersantos05@outlook.com>
2025-09-17 07:30:57 -0300

    17/09/25

--- README.md
+++
@@ -1,2 +0,0 @@
-# logicadeprogamacao
-conceitos basicos de progamação
--- exemplos.py
+++
@@ -1,9 +0,0 @@
-new_player = 'luis'
-
-players = ['ada', 'bob', 'chris', 'dan']
-
-players.insert(0, 'ana')
-
-players.append('new_player')
-
-print(players)
