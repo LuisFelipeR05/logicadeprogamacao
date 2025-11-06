f3ccbc0f260e2cd3bb2a3f3fe40a3f02780c08f1
LuisFelipeR05 <luisfelipersantos05@outlook.com>
2025-11-06 15:08:31 -0300

    06/11/25

--- Aulas/Aula11/main3.py
+++ Aulas/Aula11/main3.py
@@ -1,27 +1,36 @@
 class Carro:
-    def __init__(self,modelo,ano,cor):
+    def __init__(self, modelo, ano, cor):
         self.modelo = modelo
         self.ano = ano
         self.cor = cor
         self.velocidade = 0
-        self.frear = 0
+        self._historico = [self.velocidade]
 
     def acelerar(self):
         self.velocidade += 8
+        self._historico.append(self.velocidade)
         return self.velocidade
-    
+
     def frear(self):
         self.velocidade -= 3
+        if self.velocidade < 0:
+            self.velocidade = 0
+        self._historico.append(self.velocidade)
         return self.velocidade
 
+    def velocidade_media(self):
+        return sum(self._historico) / len(self._historico)
 
-modelo = input("Digite o modelo")
-ano = input("Digite o ano")
-cor = input("Digite o cor")
-car = Carro(modelo,ano,cor)
+modelo = input("Digite o modelo: ")
+ano = input("Digite o ano: ")
+cor = input("Digite a cor: ")
+car = Carro(modelo, ano, cor)
 
-print(car.velocidade)
+print("Velocidade inicial:", car.velocidade)
+car.acelerar()
+car.acelerar()
 car.acelerar()
 car.frear()
-print(car.velocidade)
-
+print("Velocidade final:", car.velocidade)
+print("Velocidades registradas:", car._historico)
+print("Velocidade média:", car.velocidade_media())
